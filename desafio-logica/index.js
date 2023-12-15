94d85bba357b66c294b0f08a4f513306af38657d
marlonpedrozo <marlonpedrozofelix@gmail.com>
2023-12-15 01:06:41 -0300

    terminei o desafio de codigo

--- desafio-logica/index.js
+++ desafio-logica/index.js
@@ -1,3 +1,53 @@
+// Desafio de código do Feelipão 
 
-console.log("Olá,Mundo!");
-let nome = "marlon"
+//Classificador de Nível de heroi
+
+
+
+/*
+    Se xp for menor 1000 = Ferro
+    Se xp for entre 1001 e 2000 = Bronze
+    Se xp for entre 2001 e 5000 = Prata
+    Se xp for entre 6001 e 7000 = Ouro
+    Se xp for entre 7001 e 8000 = Platina
+    Se xp for entre 8001 e 9000 = Ascendente
+    Se xp for entre 9001 e 10.000 = imortal
+    Se xp for maior ou igual a 10.001  = Radiante
+    */ 
+
+    
+    let nivelXP = 10001;
+    let classificacao = ""
+    let nomeDoHeroi = "Marlon"
+
+
+
+  if(nivelXP <1000){
+    classificacao = "Ferro",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+
+  }else if(nivelXP >1000 && nivelXP<=2000){
+    classificacao = "Bronze",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >2000 && nivelXP<=6000){
+    classificacao = "Prata",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >6000 && nivelXP<=7000){
+    classificacao = "Ouro",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >7000 && nivelXP<=8000){
+    classificacao = "Platina",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >8000 && nivelXP<=9000){
+    classificacao = "Ascendente",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >9000 && nivelXP<=10000){
+    classificacao = "Imortal",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, Tem ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else if(nivelXP >=10001){
+    classificacao = "Radiante",
+    console.log(`o Heroi de nome ${nomeDoHeroi}, recebeu ${nivelXP} em XP e está classificado como ${classificacao}`)
+  }else{
+    console.log("Algo deu errado!")
+    console.log("Por favor retorne preencha os campos corretamente")
+  };
